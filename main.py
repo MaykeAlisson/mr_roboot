916dfd5b1e8f46b09e8a6c21f434395cdc481e80
Mayke <mayke.furtado@arcom.com.br>
2021-06-26 09:11:30 -0300

    feat: create options menus

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 import time
-from decouple import config
 import telepot
+from decouple import config
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 ############################### ENV ############################################
 TOKEN = config('TELEGRAN_TOKEN')
 USER = config('TELEGRAN_USER')
-
-############################### Const ##########################################
-
-MENU = 'Menu'
 
 
 ############################### Bot ############################################
@@ -24,32 +20,84 @@ def handle(message):
         bot.sendMessage(chat_id, 'Sinto muito {0}, mas eu só falo com meu mestre!'.format(origin['first_name']))
         return
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text='Finanças', callback_data='fin')],
-        [InlineKeyboardButton(text='Develop', callback_data='dev')],
-        [InlineKeyboardButton(text='Outros', callback_data='out')],
-    ])
-
-    bot.sendMessage(chat_id, main_menu_message(), reply_markup=keyboard)
+    bot.sendMessage(chat_id, main_menu_message(), reply_markup=main_menu_keyboard())
 
 
 def on_callback_query(message):
     query_id, from_id, query_data = telepot.glance(message, flavor='callback_query')
 
-    def option(query_data):
-        return {
-            'fin': print("fin"),
-            'dev': print('dev'),
-            'out': print('out'),
-        }[query_data]
+    def option(value):
+        if value == 'fin':
+            return bot.sendMessage(USER, first_menu_message(), reply_markup=first_menu_keyboard())
+        elif value == 'dev':
+            return bot.sendMessage(USER, second_menu_message(), reply_markup=second_menu_keyboard())
+        elif value == 'out':
+            return bot.sendMessage(USER, third_menu_message(), reply_markup=third_menu_keyboard())
+        else:
+            return print("option invalid")
 
     print('Callback Query:', query_id, from_id, query_data)
 
-    bot.answerCallbackQuery(query_id, text='Got it')
+    option(query_data)
 
+    # bot.answerCallbackQuery(query_id, text='Got it')
+
+
+############################ Keyboards #########################################
+
+
+def main_menu_keyboard():
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Finance', callback_data='fin')],
+        [InlineKeyboardButton(text='Develop', callback_data='dev')],
+        [InlineKeyboardButton(text='Others', callback_data='out')],
+    ])
+    return keyboard
+
+
+def first_menu_keyboard():
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Submenu 2-1', callback_data='fin_1')],
+        [InlineKeyboardButton(text='Submenu 2-2', callback_data='fin_2')],
+        [InlineKeyboardButton(text='Menu principal', callback_data='main')]
+    ])
+    return keyboard
+
+
+def second_menu_keyboard():
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Ip', callback_data='dev_1')],
+        [InlineKeyboardButton(text='Armazenamento', callback_data='dev_2')],
+        [InlineKeyboardButton(text='Retornar menu', callback_data='main')]
+    ])
+    return keyboard
+
+
+def third_menu_keyboard():
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Submenu 3-1', callback_data='out_1')],
+        [InlineKeyboardButton(text='Submenu 3-2', callback_data='out_2')],
+        [InlineKeyboardButton(text='Menu principal', callback_data='main')]
+    ])
+    return keyboard
+
+
+############################# Messages #########################################
 
 def main_menu_message():
-    return '{0} :'.format(MENU)
+    return 'Select options :'
+
+
+def first_menu_message():
+    return 'Select options finance:'
+
+
+def second_menu_message():
+    return 'Select options develop:'
+
+
+def third_menu_message():
+    return 'Select options others:'
 
 
 bot = telepot.Bot(TOKEN)
