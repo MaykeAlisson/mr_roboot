ef6bb1744fd9e934ae67c6f49b053ed41f73ccbd
Mayke Alisson <maykealison@gmail.com>
2021-06-28 16:25:05 -0300

    feat: handler button menu

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 from decouple import config
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from handler import listener, start, argument, unknown
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+
+# from handler import listener, start, argument, unknown
 
 ############################### ENV ############################################
 TOKEN = config('TELEGRAN_TOKEN')
+
+USER = config('TELEGRAN_USER')
 
 ############################### Bot ############################################
 
@@ -12,74 +16,77 @@ updater = Updater(token=TOKEN, use_context=True)
 
 dispatcher = updater.dispatcher
 
-# Escuta text menos oque e comand
-listener_handler = MessageHandler(Filters.text & (~Filters.command), listener)
-dispatcher.add_handler(listener_handler)
-# Escuta o comando start
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-# Escuta o comando argument e recebe argumento
-argument_handler = CommandHandler('argument', argument)
-dispatcher.add_handler(argument_handler)
-# se não encontrar comando correspondente
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
+
+def listener(update, context):
+    origin = update['message']['chat']
+    if str(update.effective_chat.id) != USER:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Sinto muito {0}, mas eu só falo com meu mestre!'.format(origin['first_name']))
+        return
+
+    context.bot.send_message(chat_id=USER, text=menu_title(), reply_markup=main_menu_keyboard())
 
 
-updater.start_polling()
-# def handle(message):
-#     print(message)
-#     origin = message['chat']
-#     content_type, chat_type, chat_id = telepot.glance(message)
-#     if str(chat_id) != USER:
-#         bot.sendMessage(chat_id, 'Sinto muito {0}, mas eu só falo com meu mestre!'.format(origin['first_name']))
-#         return
-#
-#     bot.sendMessage(chat_id, main_menu_message(), reply_markup=main_menu_keyboard())
-#
+def start(update, context):
+    context.bot.send_message(chat_id=USER, text='ola mundo /start')
 
-# def on_callback_query(message):
-#     query_id, from_id, query_data = telepot.glance(message, flavor='callback_query')
-#
-#     def option(value):
-#         if value == 'fin':
-#             return bot.sendMessage(USER, first_menu_message(), reply_markup=first_menu_keyboard())
-#         elif value == 'dev':
-#             return bot.sendMessage(USER, second_menu_message(), reply_markup=second_menu_keyboard())
-#         elif value == 'out':
-#             return bot.sendMessage(USER, third_menu_message(), reply_markup=third_menu_keyboard())
-#
-#     def optionsFinance(value):
-#         pass
-#
-#     def optionsDevelop(value):
-#         pass
-#
-#     def optionsOuthers(value):
-#         pass
-#
-#     print('Callback Query:', query_id, from_id, query_data)
-#
-#     option(query_data)
-#     optionsFinance(query_data)
-#     optionsDevelop(query_data)
-#     optionsOuthers(query_data)
-#
-#     # bot.answerCallbackQuery(query_id, text='Got it')
-#
-#
-# ############################ Keyboards #########################################
-#
-#
-# def main_menu_keyboard():
-#     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-#         [InlineKeyboardButton(text='Finance', callback_data='fin')],
-#         [InlineKeyboardButton(text='Develop', callback_data='dev')],
-#         [InlineKeyboardButton(text='Others', callback_data='out')],
-#     ])
-#     return keyboard
-#
-#
+
+def argument(update, context):
+    arg = context.args
+    context.bot.send_message(chat_id=USER, text=arg)
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=USER, text='Desculpe comando não reconhecido!')
+
+
+############################ Keyboards #########################################
+
+def main_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Finance', callback_data='fin')],
+                [InlineKeyboardButton('Develop', callback_data='dev')],
+                [InlineKeyboardButton('Others', callback_data='out')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def finance_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Price', callback_data='sub_fin_1')],
+                [InlineKeyboardButton('News', callback_data='sub_fin_2')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def develop_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Ip', callback_data='sub_dev_1')],
+                [InlineKeyboardButton('Storage', callback_data='sub_dev_2')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def finance_menu(update, context):
+    context.bot.send_message(chat_id=USER, text=menu_title(), reply_markup=finance_menu_keyboard())
+
+
+def develop_menu(update, context):
+    context.bot.send_message(chat_id=USER, text=menu_title(), reply_markup=develop_menu_keyboard())
+
+
+def others_menu(update, context):
+    return context.bot.send_message(chat_id=USER, text='Em Breve!')
+
+
+def menu_title():
+    return 'Menu :'
+
+
+def listener_finance(update, context):
+    print(update)
+    print(context)
+
+
+def listener_develop(update, context):
+    print(update)
+    print(context)
+
+
 # def first_menu_keyboard():
 #     keyboard = InlineKeyboardMarkup(inline_keyboard=[
 #         [InlineKeyboardButton(text='Price', callback_data='fin_1')],
@@ -105,32 +112,25 @@ updater.start_polling()
 #         [InlineKeyboardButton(text='Menu principal', callback_data='main')]
 #     ])
 #     return keyboard
-#
-#
-# ############################# Messages #########################################
-#
-# def main_menu_message():
-#     return 'Select options :'
-#
-#
-# def first_menu_message():
-#     return 'Select options finance:'
-#
-#
-# def second_menu_message():
-#     return 'Select options develop:'
-#
-#
-# def third_menu_message():
-#     return 'Select options others:'
-#
-#
-# bot = telepot.Bot(TOKEN)
-#
-# MessageLoop(bot, {'chat': handle,
-#                   'callback_query': on_callback_query}).run_as_thread()
-# print('Listening ...')
-#
-# # Keep the program running.
-# while 1:
-#     time.sleep(10)
+
+
+# Escuta text menos oque e comand
+listener_handler = MessageHandler(Filters.text & (~Filters.command), listener)
+dispatcher.add_handler(listener_handler)
+# Escuta o comando start
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+# Escuta o comando argument e recebe argumento
+argument_handler = CommandHandler('argument', argument)
+dispatcher.add_handler(argument_handler)
+# se não encontrar comando correspondente
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+updater.dispatcher.add_handler(CallbackQueryHandler(finance_menu, pattern='fin'))
+updater.dispatcher.add_handler(CallbackQueryHandler(develop_menu, pattern='dev'))
+updater.dispatcher.add_handler(CallbackQueryHandler(others_menu, pattern='out'))
+updater.dispatcher.add_handler(CallbackQueryHandler(listener_finance, pattern='sub_fin_*'))
+updater.dispatcher.add_handler(CallbackQueryHandler(listener_develop, pattern='sub_dev_*'))
+
+updater.start_polling()
