736ac01670e3e9ff934cc2f6711fb3353693b439
Mayke <mayke.furtado@arcom.com.br>
2021-07-01 16:24:27 -0300

    feat: config tasks

--- develop.py
+++ develop.py
@@ -3,7 +3,7 @@ import os
 
 def get_ip():
     ip = os.popen("curl --max-time 60 --ipv4 icanhazip.com").read().strip()
-    # os.popen("curl http://freedns.afraid.org/dynamic/update.php?VmRDMHhhbTVlTWFvQ1p1UWpSOXU6MTgwNDk4NjA=")
+    os.popen("curl http://freedns.afraid.org/dynamic/update.php?VmRDMHhhbTVlTWFvQ1p1UWpSOXU6MTgwNDk4NjA=")
     return ip
 
 
--- handler.py
+++ handler.py
@@ -79,8 +79,7 @@ def others_menu(update, context):
     global menu
     menu = 'Others'
     context.bot.send_message(chat_id=USER, text=menu_title(), reply_markup=others_menu_keyboard())
-    # return context.bot.send_message(chat_id=USER, text='Em Breve!')
-
+    
 
 def menu_title():
     return 'Menu {0}:'.format(menu)
@@ -97,10 +96,12 @@ def listener_develop(update, context):
     query = update.callback_query.data
     print(query)
     if str(query) == 'sub_dev_1':
-        print('seu ip: ', get_ip())
+        ip = get_ip()
+        context.bot.answer_callback_query(update.callback_query.id, text='Seu ip e {0} ou mayke.mooo.com'.format(ip))
         return
     if str(query) == 'sub_dev_2':
-        print('seu storage: ', get_storage())
+        storage = get_storage()
+        context.bot.send_message(chat_id=USER, text=storage)
         return
 
 
